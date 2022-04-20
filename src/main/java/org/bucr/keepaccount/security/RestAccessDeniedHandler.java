ff4af0e78f4ce72a100290b53ee3ad002cba70d0
bucr <whut_bcr@163.com>
2022-04-21 00:48:58 +0800

    custom response for error status and other modify.
    custom response for forbidden requests and authentication entry point(but it is not working when using response advice);

--- src/main/java/org/bucr/keepaccount/security/RestAccessDeniedHandler.java
+++ src/main/java/org/bucr/keepaccount/security/RestAccessDeniedHandler.java
@@ -1,4 +1,4 @@
-package org.bucr.keepaccount.security.jwt;
+package org.bucr.keepaccount.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
@@ -8,23 +8,23 @@ import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.bucr.keepaccount.handler.ResponseData;
 import org.bucr.keepaccount.handler.ReturnCode;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
 
 @Slf4j
-public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+public class RestAccessDeniedHandler implements AccessDeniedHandler {
 
   private final ObjectMapper objectMapper = new ObjectMapper();
 
   @Override
-  public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
-      throws IOException, ServletException {
-    log.debug("Jwt authentication failed: " + authException);
-    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+  public void handle(HttpServletRequest request, HttpServletResponse response,
+      AccessDeniedException accessDeniedException) throws IOException, ServletException {
+    log.debug("Access denied, path={}, message={}", request.getPathInfo(), accessDeniedException.getMessage());
+    response.setStatus(HttpServletResponse.SC_FORBIDDEN);
     response.setContentType("text/json;charset=utf-8");
 
     ResponseData<String> responseData = new ResponseData<>();
-    responseData.setMessage(authException.getMessage());
+    responseData.setMessage(accessDeniedException.getMessage());
     responseData.setCode(ReturnCode.FORBIDDEN_ERROR.getCode());
     response.getWriter().print(objectMapper.writeValueAsString(responseData));
   }
